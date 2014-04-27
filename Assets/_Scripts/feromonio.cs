6cc57cced3296b486b0755ec28233d089fd39177
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2014-04-27 13:34:43 -0300

    Iniciando projeto

---
+++ Assets/_Scripts/feromonio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class feromonio : MonoBehaviour {
+
+    public GameObject fumacaFeromonio;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(fumacaFeromonio, transform.position, transform.rotation);
+        }
+	
+	}
+}
---
+++ Assets/_Scripts/move.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public float velocidade, velocidadeRotacao;
+    public bool gravidade;
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * velocidadeRotacao * Time.deltaTime;        
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+	}
+
+    void FixedUpdate()
+    {
+        //if (!gravidade)
+            rigidbody.AddRelativeForce(0, -20, 0);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        gravidade = false;
+        rigidbody.useGravity = false;
+
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        gravidade = true;
+        rigidbody.useGravity = true;
+
+    }
+}
